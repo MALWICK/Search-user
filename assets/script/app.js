e2a27f7581bf4328335e860fe4d9be023289d841
MALWICK <batahumphrey66@gamil.com>
2023-06-29 15:50:01 +0100

    created static data

---
+++ assets/script/app.js
@@ -0,0 +1,30 @@
+// Sample user data
+const users = [
+
+    { name: 'John Doe', age: 25 },
+    { name: 'Jane Smith', age: 30 },
+    { name: 'Mike Johnson', age: 35 },
+    { name: 'Kadji Danel', age: 18 },
+    { name: 'Alan James', age: 41 },
+    { name: 'Gaetan Vianey', age: 30 },
+    { name: 'Charles Evans', age: 55 },
+    { name: 'Bitz Boy', age: 50 },
+    { name: 'Rash Junior', age: 56 },
+    { name: 'Joshua Joshi', age: 47 },
+    { name: 'Emmanual Atangana', age: 48 },
+    { name: 'Alaric Kevin', age: 60 },
+    { name: 'Christ Godden', age: 50 },
+    { name: 'John Doe', age: 0 },
+    { name: 'Nathaneal Mesmer', age: 24 },
+    { name: 'Ange Joelle', age: 29 },
+    { name: 'Nehal Gringo', age: 53 },
+    { name: 'Nicole Mekongo', age: 43 },
+    { name: 'Jessica Keyoma', age: 32 },
+    { name: 'John Paul', age: 20 },
+    { name: 'Jones Adam', age: 17 },
+    { name: 'Johnny Alide', age: 34 },
+    { name: 'Pryan Patrick', age: 26 },
+    { name: 'Cathy Ntoulo', age: 59 },
+    { name: 'Paul Bamock', age: 15 }
+  ]
+  
